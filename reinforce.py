7eb5135c0e2d84bc6bb586fb2e21e2d48b0c825a
jinyeom <jinyeom95@gmail.com>
2017-12-01 22:23:06 -0600

    progress

--- reinforce.py
+++ reinforce.py
@@ -10,8 +10,7 @@ parser = argparse.ArgumentParser(description="TensorFlow implementation of Polic
 parser.add_argument("--n_eps", type=int, default=1000, help="Number of episodes for training.")
 parser.add_argument("--n_iter", type=int, default=10000, help="Number of iterations per episode.")
 parser.add_argument("--env_id", type=str, default="RoboschoolHumanoid-v1",
-                    help="Name of the OpenAI Roboschool environment.")
-
+                    help="OpenAI Roboschool Gym environment ID.")
 args = parser.parse_args()
 
 # Available OpenAI Gym environments from Roboschool.
@@ -26,8 +25,8 @@ env_id = args.env_id if args.env_id in roboschool_envs else "RoboschoolHumanoid-
 # Note that all environments from Roboschool has Box for observation
 # spaces and action spaces.
 env = gym.make(env_id)
-state_size = env.observation_space
-action_size = env.action_space
+state_size = env.observation_space.shape[0]
+action_size = env.action_space.shape[0]
 
 # A two-layered policy network with ReLU nonlinearity and a residual
 # connection from the input to output layer, i.e., the sum of input and
@@ -49,19 +48,20 @@ with tf.name_scope("policy"):
     with tf.name_scope("output_layer"):
         W_2 = tf.Variable(tf.truncated_normal([state_size, action_size], stddev=0.1), name="W_2")
         b_2 = tf.Variable(tf.constant(0.1, shape=[action_size]), name="b_2")
-        action = tf.matmul(hidden, W_2) + b_2
+        output = tf.matmul(hidden, W_2) + b_2
 
+for _ in range(args.n_eps):
+    state = env.reset()
+    for t in range(args.n_iter):
+        env.render()
 
-def reinforce(policy, env, episodes, iterations, render=False):
-    for i in range(episodes):
-        state = env.reset()
-        for t in range(iterations):
-            if render:
-                env.render()
-            action = policy(state)
-            state, reward, done, info = env.step(action)
+        with tf.Session() as sess:
+            action_mean = sess.run([output], feed_dict={observation: state})
+            action = np.random.normal(loc=action_mean, scale=0.1)
 
-            # TODO: implement stochastic gradient ascend with policy gradient.
+        state, reward, done, info = env.step(action)
 
-            if done:
-                break
+        # TODO: implement stochastic gradient ascend with policy gradient.
+
+        if done:
+            break
