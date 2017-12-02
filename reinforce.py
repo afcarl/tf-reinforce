fce2ccda4781fcc46c0e947a0c18bfa6747f5897
jinyeom <jinyeom95@gmail.com>
2017-12-01 21:57:54 -0600

    progress

--- reinforce.py
+++ reinforce.py
@@ -6,44 +6,62 @@ import roboschool
 import numpy as np
 import tensorflow as tf
 
-def swish(x, beta):
-    return x * tf.sigmoid(beta * x)
+parser = argparse.ArgumentParser(description="TensorFlow implementation of Policy Gradient")
+parser.add_argument("--n_eps", type=int, default=1000, help="Number of episodes for training.")
+parser.add_argument("--n_iter", type=int, default=10000, help="Number of iterations per episode.")
+parser.add_argument("--env_id", type=str, default="RoboschoolHumanoid-v1",
+                    help="Name of the OpenAI Roboschool environment.")
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
+args = parser.parse_args()
 
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+# Available OpenAI Gym environments from Roboschool.
+# If the argument environment is not in this list, run with the default
+# environment (RoboschoolHumanoid-v1).
+roboschool_envs = [s.id for s in gym.envs.registry.all() if s.id.startswith("Roboschool")]
+env_id = args.env_id if args.env_id in roboschool_envs else "RoboschoolHumanoid-v1"
 
-class Policy(object):
-    def __init__(self, input_size, hidden_size, output_size):
-        # Hidden layer
-        self.W_1 = weight_variable((input_size, hidden_size))
-        self.b_1 = bias_variable(hidden_size)
-        
-        # Beta variable for swish activation function.
-        beta_initial = tf.truncated_normal(hidden_size, stddev=0.1)
-        self.beta = tf.Variable(beta_initial)
+# Initialize OpenAI Gym environment, and configure state size,
+# hidden size, and the action size, according to the environment.
+#
+# Note that all environments from Roboschool has Box for observation
+# spaces and action spaces.
+env = gym.make(env_id)
+state_size = env.observation_space
+action_size = env.action_space
 
-        # Output layer
-        self.W_2 = weight_variable((hidden_size, output_size))
-        self.b_2 = bias_variable(output_size)
+# A two-layered policy network with ReLU nonlinearity and a residual
+# connection from the input to output layer, i.e., the sum of input and
+# the hidden layer is forwarded to the output layer.
+with tf.name_scope("policy"):
+    # Observation of the state.
+    with tf.name_scope("observation"):
+        observation = tf.placeholder(tf.float32, (None, state_size))
 
-    def __call__(self, s):
-        return self._forward(s)
+    # Hidden layer of the policy network.
+    with tf.name_scope("hidden_layer"):
+        W_1 = tf.Variable(tf.truncated_normal([state_size, state_size], stddev=0.1), name="W_1")
+        b_1 = tf.Variable(tf.constant(0.1, shape=[state_size]), name="b_1")
+        hidden = tf.nn.relu(tf.matmul(observation, W_1) + b_1)
 
-    def _forward(self, s):
-        hidden = swish(tf.matmul(s, self.W_1) + self.b_1, self.beta)
-        return tf.matmul(hidden, self.W_2) + self.b_2
+    # Output layer of the policy network.
+    # The output of this network is not the action quite yet. If this policy is stochastic, the
+    # output vector is used as the mean of action distribution (Gaussian).
+    with tf.name_scope("output_layer"):
+        W_2 = tf.Variable(tf.truncated_normal([state_size, action_size], stddev=0.1), name="W_2")
+        b_2 = tf.Variable(tf.constant(0.1, shape=[action_size]), name="b_2")
+        action = tf.matmul(hidden, W_2) + b_2
 
-def main(args):
-    return
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='TensorFlow implementation of REINFORCE')
-    parser.add_argument('--env', str, '')
-    args = parser.parse_args()
+def reinforce(policy, env, episodes, iterations, render=False):
+    for i in range(episodes):
+        state = env.reset()
+        for t in range(iterations):
+            if render:
+                env.render()
+            action = policy(state)
+            state, reward, done, info = env.step(action)
 
-    main(args)
+            # TODO: implement stochastic gradient ascend with policy gradient.
+
+            if done:
+                break
