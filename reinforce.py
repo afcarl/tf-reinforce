b3fe804c419c8be339ecffa3f7f2f05171425943
jinyeom <jinyeom95@gmail.com>
2017-12-29 06:34:25 -0600

    progress; doens't work yet

--- reinforce.py
+++ reinforce.py
@@ -1,21 +1,37 @@
-""" TensorFlow implementation of policy gradient (REINFORCE). """
+""" TensorFlow implementation of REINFORCE. """
 
+import os
+# Silence TensorFlow warnings
+os.environ["TF_CPP_MIN_LOG_LEVEL"]="2"
+import tqdm
 import argparse
 import gym
 import roboschool
 import numpy as np
 import tensorflow as tf
-# quick fix for roboschool
+# Quick fix for roboschool
 from OpenGL import GL
 
-def weight_variable(shape, name):
+def weight_variable(shape):
     """ Return a tensorflow.Variable for dense layer weights. """
     init_val = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(init_val, name=name)
+    return tf.Variable(init_val)
 
-def bias_variable(shape, name):
+def bias_variable(shape):
     """ Return a tensorflow.Variable for bias. """
-    return tf.Variable(tf.constant(0.1, shape=shape), name=name)
+    return tf.Variable(tf.constant(0.1, shape=shape))
+
+def variable_summaries(var):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope("summaries"):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar("mean", mean)
+        with tf.name_scope("stddev"):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+            tf.summary.scalar("stddev", stddev)
+            tf.summary.scalar("max", tf.reduce_max(var))
+            tf.summary.scalar("min", tf.reduce_min(var))
+            tf.summary.histogram("histogram", var)
 
 class GaussianPolicy(object):
     """ Gaussian policy network with three layers. """
@@ -23,46 +39,68 @@ class GaussianPolicy(object):
     def __init__(self, arch, sigma_sq):
         if len(arch) != 4:
             raise ValueError("Policy network architecture must contain 4 ints")
+        # Store architecture information.
+        self.obs_size = arch[0]
+        self.hidden_size_1 = arch[1]
+        self.hidden_size_2 = arch[2]
+        self.act_size = arch[3]
 
         # Observation represents state argument (s) of the policy (pi).
         # In terms of a neural network, this is the input layer of the policy network.
         with tf.name_scope("observation"):
-            self.observation = tf.placeholder(tf.float32, (None, arch[0]))
+            self.observation = tf.placeholder(tf.float32, [None, self.obs_size])
 
         # First hidden layer of the policy network.
         with tf.name_scope("hidden_1"):
-            W_1 = weight_variable((arch[0], arch[1]), "W_1")
-            b_1 = bias_variable((arch[1], ), "b_1")
-            a_1 = tf.nn.relu(tf.matmul(observation, W_1) + b_1)
+            with tf.name_scope("W_1"):
+                W_1 = weight_variable([self.obs_size, self.hidden_size_1])
+                variable_summaries(W_1)
+            with tf.name_scope("b_1"):
+                b_1 = bias_variable([self.hidden_size_1])
+                variable_summaries(b_1)
+            a_1 = tf.nn.relu(tf.matmul(self.observation, W_1) + b_1)
 
         # Second hidden layer of the policy network.
         with tf.name_scope("hidden_2"):
-            W_2 = weight_variable((arch[1], arch[2]), "W_2")
-            b_2 = bias_variable((arch[2], ), "b_2")
+            with tf.name_scope("W_2"):
+                W_2 = weight_variable([self.hidden_size_1, self.hidden_size_2])
+                variable_summaries(W_2)
+            with tf.name_scope("b_2"):
+                b_2 = bias_variable([self.hidden_size_2])
+                variable_summaries(b_2)
             a_2 = tf.nn.relu(tf.matmul(a_1, W_2) + b_2)
 
         # Output layer of the policy network.
         # This layer returns the mean vector of the action distribution.
         with tf.name_scope("action_mean"):
-            W_3 = weight_variable((arch[2], arch[3]), "W_3")
-            b_3 = bias_variable((arch[3], ), "b_3")
-            mu = tf.matmul(a_2, W_3) + b_3
+            with tf.name_scope("W_3"):
+                W_3 = weight_variable([self.hidden_size_2, self.act_size])
+                variable_summaries(W_3)
+            with tf.name_scope("b_3"):
+                b_3 = bias_variable([self.act_size])
+                variable_summaries(b_3)
+            with tf.name_scope("mu"):
+                self.mu = tf.matmul(a_2, W_3) + b_3
+                tf.summary.histogram("mu", self.mu)
 
         # Use the output of the policy network as the mean of the action disctribution.
         # The action is sampled from the distribution to compute gradient.
         with tf.name_scope("action"):
-            self.pi_sa = tf.distributions.Normal(mu, sigma_sq)
+            self.pi_sa = tf.distributions.Normal(self.mu, sigma_sq)
             self.action = self.pi_sa.sample()
+            tf.summary.histogram("action", self.action)
 
-        # Reward placeholder for updating the policy during training.
-        with tf.name_scope("reward"):
-            self.reward = tf.placeholder(tf.float32, (None, ))
+        # Value placeholder for updating the policy during training.
+        # For REINFORCE, the return value G_t for each time step t is used for value.
+        with tf.name_scope("value"):
+            self.value = tf.placeholder(tf.float32, [None, 1])
 
         # Loss function of Gaussian policy.
         # L = log(pi(a | s)) * V(s, a)
         with tf.name_scope("loss"):
             log_pi_sa = self.pi_sa.log_prob(self.action)
-            self.loss = tf.reduce_mean(log_pi_sa * self.reward)
+            self.loss = tf.reduce_mean(log_pi_sa * self.value)
+            tf.summary.scalar("objective", -self.loss)
 
         # Training operator with Adam optimizer.
         # Note that the optimizer minimizes the negative loss, i.e., maximizes the loss.
@@ -71,17 +109,19 @@ class GaussianPolicy(object):
             self.train_op = optimizer.minimize(-self.loss)
 
         self.sess = tf.Session()
+        self.merged = tf.summary.merge_all()
+        self.summary_writer = tf.summary.FileWriter("./logs", self.sess.graph)
         self.sess.run(tf.global_variables_initializer())
 
     def __call__(self, state):
         """ Sample from the action distribution given the argument state. """
         return self.sess.run(self.action, feed_dict={self.observation: state})
 
-    def learn(self, state, reward):
+    def learn(self, episode, state, value):
         """ Optimize the policy given a set of states and corresponding rewards. """
-        feed_dict = {self.observation: state, self.reward: reward}
-        loss, _ = self.sess.run([self.loss, self.train_op], feed_dict=feed_dict)
-        return loss
+        feed_dict = {self.observation: state, self.value: value}
+        summary, _ = self.sess.run([self.merged, self.train_op], feed_dict=feed_dict)
+        self.summary_writer.add_summary(summary, episode)
 
 class Episode(object):
     """ Sequence of (state, action, reward). """
@@ -126,11 +166,44 @@ class Episode(object):
     @property
     def rewards(self):
         """ Return the reward vector. """
-        return np.stack(self._reward_buffer, axis=0)
+        return np.stack(self._reward_buffer, axis=0)[:, np.newaxis]
+
+    def returns(self, gamma):
+        """ Return a vector of return values at each time step. """
+        ret = [self._reward_buffer[0]]
+        for t in range(1, self._len):
+            ret.append(ret[t - 1] + (gamma ** t) * self._reward_buffer[t])
+        norm = np.linalg.norm(ret)
+        if norm == 0:
+            return np.stack(ret, axis=0)[:, np.newaxis]
+        return np.stack(ret, axis=0)[:, np.newaxis] / norm 
+
+
+def reinforce(pi, env):
+    """ Execute REINFORCE (Monte Carlo policy gradient) to train the argument policy (pi) in
+    the argument environment. """
+    for ep in tqdm.trange(args.n_eps):
+        episode = Episode()
+        state = env.reset()
+        done = False
+        while not done:
+            action = pi(state[np.newaxis, :]).flatten()
+            state_, reward, done, info = env.step(action)
+            episode.append(state, action, reward)
+            state = state_
+        pi.learn(ep, episode.states, episode.rewards)
+
+def rollout(pi, env, render=True):
+    """ Make the argument policy interact with the environment. """
+    state = env.reset()
+    done = False
+    while not done:
+        if render:
+            env.render()
+        action = pi(state[np.newaxis, :]).flatten()
+        state, reward, done, info = env.step(action)
 
 def main(args):
-    """ Execute REINFORCE (Monte Carlo policy gradient). """
-
     # Available OpenAI Gym environments from Roboschool.
     # If the argument environment is not in this list, run with the default
     # environment (RoboschoolHumanoid-v1).
@@ -147,25 +220,17 @@ def main(args):
         args.hidden[1],
         env.action_space.shape[0]
     ]
-    
+
+    # Print summary of the experiment setup.
+    print("Environment ID:", env_id)
+    print("Policy architecture:", arch)
+
+    # Run REINFORCE.
     pi = GaussianPolicy(arch, args.var)
-    for ep in range(args.n_eps):
-        episode = Episode()
-        state = env.reset()
-        while True:
-            action = pi(state[np.newaxis, :]).reshape((arch[3]))
-            state_, reward, done, info = env.step(action)
-            episode.append(state, action, reward)
-            state = state_
-            if done:
-                states = trajectory.states
-                rewards = trajectory.rewards
-                pi.optimize(states, rewards)
-                # Only print the training process 10 times.
-                if ep % (args.n_eps / 10) == 0:
-                    ret = tf.reduce_sum(rewards).eval(pi.sess)
-                    print("Episode %d: return = %f" % (ep, ret))
-                break
+    reinforce(pi, env)
+
+    # Visually test the trained policy.
+    rollout(pi, env)    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="TensorFlow implementation of Policy Gradient")
