86f2b5efd2dfba6a608948451f7b2bb4904ffcad
jinyeom <jinyeom95@gmail.com>
2018-01-09 10:15:54 -0600

    progress

--- experiment.py
+++ experiment.py
@@ -1,8 +1,10 @@
 import subprocess
 
-subprocess.run(["python", "reinforce.py",
+subprocess.run(["python", "reinforce_roboschool.py",
                 "--hidden", "16", "16",
                 "--n_eps", "5000",
+                "--n_iter", "10000",
                 "--gamma", "0.99",
+                "--sigma_sq", "0.01",
                 "--lr", "0.001",
                 "--env_id", "RoboschoolAnt-v1"])
